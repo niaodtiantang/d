d304cd60289970979bedc95cd2a1d1c8b857e9b1
niaodtiantang <80442013+niaodtiantang@users.noreply.github.com>
2022-05-14 06:56:19 +0800

    Update index.js

--- file/js/页脚/index.js
+++ file/js/页脚/index.js
@@ -1,4 +1,6 @@
 let d = new Date(); //获取当前时间
 let year = d.getFullYear(); //获取当前年份
-let n ='<a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener">粤ICP备2022003293号-1</a><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030502008589" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="https://cdn.jsdelivr.net/gh/niaodtiantang/d@master/beianimg.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">粤公网安备 44030502008589号</p></a><b>©2022 - '+year+' niaodtiantang.com all rights reserved.</b>';
+let n ='<a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener">粤ICP备2022003293号-1</a><a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030502008589" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="https://cdn.jsdelivr.net/gh/niaodtiantang/d@master/beianimg.png" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">粤公网安备 44030502008589号</p></a>';
+let b ='<b>©2022 - '+year+' niaodtiantang.com all rights reserved.</b>';
 document.write(n);
+document.write(b);
