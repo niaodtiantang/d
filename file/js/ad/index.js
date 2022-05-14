45ba558f2f44809be556ab9d09fa902a65773bec
niaodtiantang <80442013+niaodtiantang@users.noreply.github.com>
2022-05-14 14:31:33 +0800

    Update index.js

--- file/js/ad/index.js
+++ file/js/ad/index.js
@@ -1,6 +1,6 @@
 $.get("https://v1.hitokoto.cn/?encode=text",function(data,status){
         alert("数据: " + data + "\n状态: " + status);
         if (status=='success'){
-        document.writeln('');
+        document.writeln('<div class="mdui-card"><div class="mdui-card-media"><img src="https://api.dujin.org/bing/1920.php"/><div class="mdui-card-media-covered mdui-card-media-covered-transparent"><div class="mdui-card-primary"><div class="mdui-card-primary-title">Title</div><div class="mdui-card-primary-subtitle">Subtitle</div></div></div></div>');
         }
     });
