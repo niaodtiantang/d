6ccf33a111fc48c9e7ae72796f377af932d26053
niaodtiantang <80442013+niaodtiantang@users.noreply.github.com>
2022-05-14 14:28:58 +0800

    Update index.js

--- file/js/ad/index.js
+++ file/js/ad/index.js
@@ -1,3 +1,6 @@
 $.get("https://v1.hitokoto.cn/?encode=text",function(data,status){
         alert("数据: " + data + "\n状态: " + status);
+        if (status=='success'){
+        document.writeln('');
+        }
     });
