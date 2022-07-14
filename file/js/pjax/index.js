077bc755baad56699150a0e646c7e654e4f79544
niaodtiantang <80442013+niaodtiantang@users.noreply.github.com>
2022-07-15 07:56:42 +0800

    Update index.js

--- file/js/pjax/index.js
+++ file/js/pjax/index.js
@@ -2,7 +2,7 @@ $(function(){
     //这里注意
     //live只有jquery - 1.8支持
     //如果是jquery - 2.0 以上，请使用on
-    $('a').live('click',function(){
+    $('a').on('click',function(){
         var url        =    $(this).attr('href');
         var target    =    $(this).attr('target');
         //如果是javascript:;证明是无需使用pjax跳转页面的链接
@@ -17,6 +17,10 @@ $(function(){
             $.pjax({
                 url: url,
                 container: 'body'
+            })
+            $.pjax({
+                url: url,
+                container: 'head'
             })
         }
     });
