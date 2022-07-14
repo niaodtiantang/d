6acc90a18d9a3c796a69cf024162d463fdbf356d
niaodtiantang <80442013+niaodtiantang@users.noreply.github.com>
2022-07-15 07:49:23 +0800

    Delete file/js/ad directory

--- file/js/ad/index.js
+++
@@ -1,6 +0,0 @@
-$.get("https://v1.hitokoto.cn/?encode=text",function(data,status){
- 
-        if (status=='success'){
-        document.writeln('<div class="mdui-card"><div class="mdui-card-media"><img src="https://api.dujin.org/bing/1920.php"/><div class="mdui-card-media-covered mdui-card-media-covered-transparent"><div class="mdui-card-primary"><div class="mdui-card-primary-title"><i class="mdui-icon material-icons">textsms</i>'+data+'</div></div></div></div>');
-        }
-    });
